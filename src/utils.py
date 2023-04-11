1eecdd0dc9fb93969836af361ef74597f369cc68
BhavithaKankala <bhavithakankala1@gmail.com>
2023-04-11 23:21:55 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,25 @@ def save_object(file_path,obj):
 
         with open(file_path,"wb") as file_obj:
             pickle.dump(obj, file_obj)
-            
+
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_model(x_train, y_train, x_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            #train model
+            model.fit(x_train,y_train)
+
+            #predict testing data
+            y_test_pred = model.predict(x_test)
+
+            #get r2 score for train and test data
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except :
+        pass
